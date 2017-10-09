e35534a2888c721d63bc6d942f8e1587981c0ff8
langshiquan <langshiquan@baidu.com>
2017-10-09 19:24:43 +0800

    init

---
+++ src/main/java/com/baidu/langshiquan/Main.java
@@ -0,0 +1,10 @@
+package com.baidu.langshiquan;
+
+/**
+ * Created by langshiquan on 17/10/9.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello,jedis");
+    }
+}
